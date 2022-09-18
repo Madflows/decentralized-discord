9238aa56b08aaaa2f31147693ddd434b0240365e
Folarin Lawal <lawalfolarin6@gmail.com>
2022-09-18 11:49:34 +0000

    disbar component created

---
+++ client/components/SideBar.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import {  useEffect, useState} from "react";
+import { useRouter } from 'next/router';
+import styles from '../styles/Sidebar.module.css'
+
+const SideBar = () => {
+  return (
+    <div className={styles.wrapper}>SideBar</div>
+  )
+}
+
+export default SideBar
