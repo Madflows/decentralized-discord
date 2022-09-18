f32531a79111c24b9027bc41158c66cabd34731a
Temmy Lizzy <temmylizzy59@gmail.com>
2022-09-18 16:21:39 +0000

    stil on the conversation list

--- client/components/ConversationList.jsx
+++ client/components/ConversationList.jsx
@@ -6,7 +6,39 @@ import Image from "next/image";
 import friends from "../assets/icons/friends.svg";
 import nitro from "../assets/icons/nitro.svg";
 
+import avatar1 from '../assets/avatar-1.webp'
+import avatar2 from '../assets/avatar-2.png'
+import avatar3 from '../assets/avatar-3.webp'
+import avatar4 from '../assets/avatar-4.webp'
+
+const DUMMY_DMS = [
+    {
+      id: 1,
+      name: 'Bill Gates',
+      avatar: avatar1,
+    },
+    {
+      id: 2,
+      name: 'Mark Zuckerberg',
+      avatar: avatar2,
+    },
+    {
+      id: 3,
+      name: 'Dependabot',
+      avatar: avatar3,
+    },
+    {
+      id: 4,
+      name: 'Defendabot',
+      avatar: avatar4,
+    },
+  ]
+
+
 const ConversationList = () => {
+    const [dms, setDms] = useState(DUMMY_DMS)
+
+
   return (
     <div className={styles.conversations}>
       <div className={styles.conversationListTop}>
@@ -37,6 +69,15 @@ const ConversationList = () => {
           </div>
           <p>Nitro</p>
         </button>
+      <div className={styles.dmTitle}>
+        <h2>Direct Messages</h2>
+      </div>
+
+      {
+        dms.map((dm, index) => (
+            <div key={index}>Dm Card</div>
+        ))
+      }
       </div>
     </div>
   );
